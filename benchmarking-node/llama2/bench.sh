b1dfcba6cb379fd8336cd60d0d132db322887936
aaronmunsters <aaronmunstersbxl@gmail.com>
2024-08-01 16:00:39 +0200

    Enable benchmarking for llama library

--- benchmarking-node/llama2/bench.sh
+++ benchmarking-node/llama2/bench.sh
@@ -1,35 +1,62 @@
-mkdir -p working-dir
-cd working-dir
+#!/usr/bin/env bash
 
-# TODO: pin these to a set version
+mkdir -p working-dir && cd "$_" || exit
+
 git_url_llama2_c="https://github.com/karpathy/llama2.c"
-git_url_wasi_libc="https://github.com/WebAssembly/wasi-libc.git"
-
+git_url_llama2_c_version="350e04fe35433e6d2941dce5a1f53308f87058eb"
 folder_llama2_c="llama2.c"
 
-# 1. Download Llama2.c
-echo "Git clone Llama2.c if not cloned yet"
+#     ####################################################################
+echo "##### 1. Git clone Llama2.c at specific hash if not cloned yet #####"
+#     ####################################################################
 if [[ ! -d ${folder_llama2_c} ]]; then
-    git clone "${git_url_llama2_c}" "${folder_llama2_c}"
+    mkdir ${folder_llama2_c}
+    cd ${folder_llama2_c} || exit
+    # src: https://stackoverflow.com/a/43136160
+    git init --quiet && git remote add origin ${git_url_llama2_c}
+    # clones +/- 2.2M of data
+    git fetch --depth 1 origin ${git_url_llama2_c_version} --quiet
+    git checkout FETCH_HEAD --quiet
 else
-    git -C "${folder_llama2_c}" pull
+    cd ${folder_llama2_c} || exit
+    git checkout FETCH_HEAD --quiet
 fi
 
-# 2. Go to Llama2.c repository
-cd ${folder_llama2_c}
+#     ###############################
+echo "##### 2. Patch the C code #####"
+#     ###############################
+# test if run.c has already changes applied (src: https://stackoverflow.com/a/25149786)
+if [[ `git status run.c --porcelain` ]]; then git restore run.c; fi
 
-# 3. Fetch the LLM
-model_bin_path="model.bin"
-wget -O ${model_bin_path} --no-clobber https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin
+cp ../../patch_for_benchmarks.patch .
+git apply patch_for_benchmarks.patch
+rm patch_for_benchmarks.patch
 
-# 4. Compile to wasm
-llama_js_bin_name="llama2_c.js"
+#     ############################
+echo "##### 3. Fetch the LLM #####"
+#     ############################
+
+#   options: "15M" (58M of data), "42M" (159M of data), "110M" (418M of data)
+model_bin_path="stories15M.bin"
+wget -O ${model_bin_path} --no-clobber --no-verbose --quiet https://huggingface.co/karpathy/tinyllamas/resolve/main/${model_bin_path}
+
+#     ##############################
+echo "##### 4. Compile to wasm #####"
+#     ##############################
+llama_bin_name="llama2_c"
+llama_js_bin_name=${llama_bin_name}.js # Output a NodeJS accessible script
 emcc run.c \
-    -o ${llama_js_bin_name}                                       `# Output a NodeJS accessible script`  \
-    -sALLOW_MEMORY_GROWTH                                         `# Allow memory to grow at runtime`    \
-    -lm                                                           `# Linking the math library`           \
-    -O3                                                           `# Compiling for maximum perfomance`   \
-    --preload-file ${model_bin_path} --preload-file tokenizer.bin `# Allow binary to access the llm model & the tokenizer`
+    -o ${llama_js_bin_name} \
+    -sALLOW_MEMORY_GROWTH                                         `# Allow memory to grow at runtime`                       \
+    -O3                                                           `# Compiling for maximum perfomance`                      \
+    -lm                                                           `# Linking the math library`                              \
+    --preload-file ${model_bin_path} --preload-file tokenizer.bin `# Allow binary to access the llm model & the tokenizer`  \
+    -s EXPORTED_FUNCTIONS=_inference                                                                                        \
+    -s EXPORTED_RUNTIME_METHODS='["cwrap","FS","wasmExports"]'
 
-# Execute!
-node ${llama_js_bin_name} ${model_bin_path}
+#     #######################
+echo "##### 5. Execute! #####"
+#     #######################
+cp ../../execute.js .
+
+node execute.js ${llama_js_bin_name} ${model_bin_path} # <-- optionally include `verbose` argument
