fc0593dc2b5583b4a634f41208ba82e5266229c5
aaronmunsters <aaronmunstersbxl@gmail.com>
2024-10-21 16:20:21 +0200

    Make instruction-mix analysis location-compatible

--- benchmarking-node/input-analyses/rust/instruction-mix/src/lib.rs
+++ benchmarking-node/input-analyses/rust/instruction-mix/src/lib.rs
@@ -53,30 +53,30 @@ enum InstructionIndex {
     LoopPost,
 }
 
-advice! { if_                (c: PathContinuation, _ic: IfThenElseInputCount, _ia: IfThenElseArity   ) { inc_instr(InstructionIndex::If);               c                         } }
-advice! { if_post            (                                                                       ) { inc_instr(InstructionIndex::IfPost);                                     } }
-advice! { if_then            (c: PathContinuation, _ic: IfThenInputCount, _ia: IfThenArity           ) { inc_instr(InstructionIndex::IfThen);           c                         } }
-advice! { if_then_post       (                                                                       ) { inc_instr(InstructionIndex::IfThenPost);                                 } }
-advice! { br                 (_l: BranchTargetLabel                                                  ) { inc_instr(InstructionIndex::Br);                                         } }
-advice! { br_if              (c : ParameterBrIfCondition, _l : ParameterBrIfLabel                    ) { inc_instr(InstructionIndex::BrIf);             c                         } }
-advice! { br_table           (bt: BranchTableTarget, _e: BranchTableEffective, _d: BranchTableDefault) { inc_instr(InstructionIndex::BrIfTable);        bt                        } }
-advice! { select             (c: PathContinuation                                                    ) { inc_instr(InstructionIndex::Select);           c                         } }
-advice! { call pre           (_t : FunctionIndex                                                     ) { inc_instr(InstructionIndex::CallPre);                                    } }
-advice! { call post          (_t : FunctionIndex                                                     ) { inc_instr(InstructionIndex::CallPost);                                   } }
-advice! { call_indirect pre  (t: FunctionTableIndex, _f: FunctionTable                               ) { inc_instr(InstructionIndex::CallIndirectPre);  t                         } }
-advice! { call_indirect post (_t: FunctionTable                                                      ) { inc_instr(InstructionIndex::CallIndirectPost);                           } }
-advice! { unary generic      (opt: UnaryOperator, opnd: WasmValue                                    ) { inc_instr(InstructionIndex::Unary);            opt.apply(opnd)           } }
-advice! { binary generic     ( opt: BinaryOperator, l_opnd: WasmValue, r_opnd: WasmValue             ) { inc_instr(InstructionIndex::Binary);           opt.apply(l_opnd, r_opnd) } }
-advice! { drop               (                                                                       ) { inc_instr(InstructionIndex::Drop);                                       } }
-advice! { return_            (                                                                       ) { inc_instr(InstructionIndex::Return);                                     } }
-advice! { const_ generic     (v: WasmValue                                                           ) { inc_instr(InstructionIndex::Const);            v                         } }
-advice! { local generic      (v: WasmValue, _i: LocalIndex, _l: LocalOp                              ) { inc_instr(InstructionIndex::Local);            v                         } }
-advice! { global generic     (v: WasmValue, _i: GlobalIndex, _g: GlobalOp                            ) { inc_instr(InstructionIndex::Global);           v                         } }
-advice! { load generic       (i: LoadIndex, o: LoadOffset, op: LoadOperation                         ) { inc_instr(InstructionIndex::Load);             op.perform(&i, &o)        } }
-advice! { store generic      (i: StoreIndex, v: WasmValue, o: StoreOffset, op: StoreOperation        ) { inc_instr(InstructionIndex::Store);            op.perform(&i, &v, &o);   } }
-advice! { memory_size        (s: WasmValue, _i: MemoryIndex                                          ) { inc_instr(InstructionIndex::MemorySize);       s                         } }
-advice! { memory_grow        (a: WasmValue, i: MemoryIndex                                           ) { inc_instr(InstructionIndex::MemoryGrow);       i.grow(a)                 } }
-advice! { block pre          (_bi: BlockInputCount, _ba: BlockArity                                  ) { inc_instr(InstructionIndex::BlockPre);                                   } }
-advice! { block post         (                                                                       ) { inc_instr(InstructionIndex::BlockPost);                                  } }
-advice! { loop_ pre          (_li: LoopInputCount, _la: LoopArity                                    ) { inc_instr(InstructionIndex::LoopPre);                                    } }
-advice! { loop_ post         (                                                                       ) { inc_instr(InstructionIndex::LoopPost);                                   } }
+advice! { if_                (c: PathContinuation, _ic: IfThenElseInputCount, _ia: IfThenElseArity   , _l: Location) { inc_instr(InstructionIndex::If);               c                         } }
+advice! { if_post            (                                                                         _l: Location) { inc_instr(InstructionIndex::IfPost);                                     } }
+advice! { if_then            (c: PathContinuation, _ic: IfThenInputCount, _ia: IfThenArity           , _l: Location) { inc_instr(InstructionIndex::IfThen);           c                         } }
+advice! { if_then_post       (                                                                         _l: Location) { inc_instr(InstructionIndex::IfThenPost);                                 } }
+advice! { br                 (_l: BranchTargetLabel                                                  , _l: Location) { inc_instr(InstructionIndex::Br);                                         } }
+advice! { br_if              (c : ParameterBrIfCondition, _l : ParameterBrIfLabel                    , _l: Location) { inc_instr(InstructionIndex::BrIf);             c                         } }
+advice! { br_table           (bt: BranchTableTarget, _e: BranchTableEffective, _d: BranchTableDefault, _l: Location) { inc_instr(InstructionIndex::BrIfTable);        bt                        } }
+advice! { select             (c: PathContinuation                                                    , _l: Location) { inc_instr(InstructionIndex::Select);           c                         } }
+advice! { call pre           (_t : FunctionIndex                                                     , _l: Location) { inc_instr(InstructionIndex::CallPre);                                    } }
+advice! { call post          (_t : FunctionIndex                                                     , _l: Location) { inc_instr(InstructionIndex::CallPost);                                   } }
+advice! { call_indirect pre  (t: FunctionTableIndex, _f: FunctionTable                               , _l: Location) { inc_instr(InstructionIndex::CallIndirectPre);  t                         } }
+advice! { call_indirect post (_t: FunctionTable                                                      , _l: Location) { inc_instr(InstructionIndex::CallIndirectPost);                           } }
+advice! { unary generic      (opt: UnaryOperator, opnd: WasmValue                                    , _l: Location) { inc_instr(InstructionIndex::Unary);            opt.apply(opnd)           } }
+advice! { binary generic     ( opt: BinaryOperator, l_opnd: WasmValue, r_opnd: WasmValue             , _l: Location) { inc_instr(InstructionIndex::Binary);           opt.apply(l_opnd, r_opnd) } }
+advice! { drop               (                                                                         _l: Location) { inc_instr(InstructionIndex::Drop);                                       } }
+advice! { return_            (                                                                         _l: Location) { inc_instr(InstructionIndex::Return);                                     } }
+advice! { const_ generic     (v: WasmValue                                                           , _l: Location) { inc_instr(InstructionIndex::Const);            v                         } }
+advice! { local generic      (v: WasmValue, _i: LocalIndex, _l: LocalOp                              , _l: Location) { inc_instr(InstructionIndex::Local);            v                         } }
+advice! { global generic     (v: WasmValue, _i: GlobalIndex, _g: GlobalOp                            , _l: Location) { inc_instr(InstructionIndex::Global);           v                         } }
+advice! { load generic       (i: LoadIndex, o: LoadOffset, op: LoadOperation                         , _l: Location) { inc_instr(InstructionIndex::Load);             op.perform(&i, &o)        } }
+advice! { store generic      (i: StoreIndex, v: WasmValue, o: StoreOffset, op: StoreOperation        , _l: Location) { inc_instr(InstructionIndex::Store);            op.perform(&i, &v, &o);   } }
+advice! { memory_size        (s: WasmValue, _i: MemoryIndex                                          , _l: Location) { inc_instr(InstructionIndex::MemorySize);       s                         } }
+advice! { memory_grow        (a: WasmValue, i: MemoryIndex                                           , _l: Location) { inc_instr(InstructionIndex::MemoryGrow);       i.grow(a)                 } }
+advice! { block pre          (_bi: BlockInputCount, _ba: BlockArity                                  , _l: Location) { inc_instr(InstructionIndex::BlockPre);                                   } }
+advice! { block post         (                                                                         _l: Location) { inc_instr(InstructionIndex::BlockPost);                                  } }
+advice! { loop_ pre          (_li: LoopInputCount, _la: LoopArity                                    , _l: Location) { inc_instr(InstructionIndex::LoopPre);                                    } }
+advice! { loop_ post         (                                                                         _l: Location) { inc_instr(InstructionIndex::LoopPost);                                   } }
--- wastrumentation-instr-lib/tests/instruction-mix.rs
+++ wastrumentation-instr-lib/tests/instruction-mix.rs
@@ -1,10 +1,11 @@
 // Rust STD
-use std::path::{absolute, PathBuf};
+use std::path::absolute;
 
 // Wastrumentation imports
 use rust_to_wasm_compiler::WasiSupport;
 use wastrumentation::{compiler::Compiles, Configuration, PrimaryTarget, Wastrumenter};
 use wastrumentation_instr_lib::lib_compile::assemblyscript::compiler::Compiler as ASCompiler;
+use wastrumentation_instr_lib::lib_compile::rust::options::{ManifestSource, RustSourceCode};
 use wastrumentation_instr_lib::lib_compile::rust::{
     compiler::Compiler,
     options::{CompilerOptions, RustSource, RustSource::Manifest},
@@ -17,20 +18,20 @@ use wasmtime::{Config, Engine, Linker, Module, Store};
 use wasmtime_wasi::preview1::{self, WasiP1Ctx};
 use wasmtime_wasi::WasiCtxBuilder;
 
-fn compile_input_program(input_program: impl Into<PathBuf>) -> Vec<u8> {
+fn compile_input_program() -> Vec<u8> {
     Compiler::setup_compiler()
         .unwrap()
         .compile(&CompilerOptions {
             profile: rust_to_wasm_compiler::Profile::Release,
-            source: RustSource::Manifest(
+            source: RustSource::SourceCode(
                 WasiSupport::Disabled,
-                absolute(input_program.into()).unwrap(),
+                ManifestSource(INPUT_PROGRAM_MANIFEST.into()),
+                RustSourceCode(INPUT_PROGRAM_SOURCE.into()),
             ),
         })
         .unwrap()
 }
 
-const PATH_INPUT_PROGRAM: &str = "./tests/input-programs/rust/rust-taint-input-program/Cargo.toml";
 const PATH_INPUT_ANLYSIS: &str = "./tests/analyses/rust/instruction-mix/Cargo.toml";
 
 #[test]
@@ -44,7 +45,37 @@ fn test_analysis() {
         ASCompiler::setup_compiler().expect("Setup AssemblyScript compiler");
 
     let source = Manifest(WasiSupport::Enabled, absolute(PATH_INPUT_ANLYSIS).unwrap());
-    let hooks = Hook::all_hooks();
+    let hooks = vec![
+        Hook::IfThenElse,
+        Hook::IfThenElsePost,
+        Hook::IfThen,
+        Hook::IfThenPost,
+        Hook::Branch,
+        Hook::BranchIf,
+        Hook::BranchTable,
+        Hook::Select,
+        Hook::CallPre,
+        Hook::CallPost,
+        Hook::CallIndirectPre,
+        Hook::CallIndirectPost,
+        Hook::Unary,
+        Hook::Binary,
+        Hook::Drop,
+        Hook::Return,
+        Hook::Const,
+        Hook::Local,
+        Hook::Global,
+        Hook::Load,
+        Hook::Store,
+        Hook::MemorySize,
+        Hook::MemoryGrow,
+        Hook::BlockPre,
+        Hook::BlockPost,
+        Hook::LoopPre,
+        Hook::LoopPost,
+    ]
+    .into_iter()
+    .collect();
     let analysis = RustAnalysisSpec { source, hooks }.into();
 
     let configuration = Configuration {
@@ -52,7 +83,7 @@ fn test_analysis() {
         primary_selection: Some(PrimaryTarget::Analysis),
     };
 
-    let input_program = compile_input_program(PATH_INPUT_PROGRAM);
+    let input_program = compile_input_program();
     let wastrumenter = Wastrumenter::new(instrumentation_compiler.into(), analysis_compiler.into());
     let wastrumented = wastrumenter
         .wastrument(&input_program, analysis, &configuration)
@@ -100,15 +131,55 @@ fn test_analysis() {
         .unwrap();
 
     // Invoke
-    match entry_point_function.call(&mut store, 10) {
-        Err(err) => {
-            println!("Error: {err}");
-            println!("STDOUT: \n{}", String::from_utf8_lossy(&stdout.contents()));
-            println!("STDERR: \n{}", String::from_utf8_lossy(&stderr.contents()));
-        }
-        Ok(res) => {
-            println!("STDOUT: \n{}", String::from_utf8_lossy(&stdout.contents()));
-            println!("Success, outcome = {res:?} (is this (i32.const 2)) ?");
-        }
-    };
+    assert_eq!(entry_point_function.call(&mut store, 10).unwrap(), 13689);
 }
+
+const INPUT_PROGRAM_SOURCE: &str = r#"
+static mut GLOBAL_COUNT_TWO: i32 = 0;
+
+#[no_mangle]
+pub extern "C" fn g(input: i32) -> i32 {
+    unsafe { GLOBAL_COUNT_TWO += 2 };
+    let a = input + 1;
+    let b = a * 10;
+    let c = b - 25;
+    #[allow(arithmetic_overflow)]
+    return c >> 1;
+}
+
+#[no_mangle]
+pub extern "C" fn f(input: i32) -> i32 {
+    if input == 0 {
+        return 12345;
+    };
+    let x = 2 * input;
+    let y = 5 + x;
+    let z = g(y) * g(y);
+    return z;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_f() {
+        assert_eq!(1555009, f(123));
+    }
+}
+"#;
+
+const INPUT_PROGRAM_MANIFEST: &str = r#"
+[package]
+name = "rust-taint-input-program"
+version = "0.1.0"
+edition = "2021"
+[lib]
+crate-type = ["cdylib"]
+[profile.release]
+strip = true
+lto = true
+panic = "abort"
+[dependencies]
+[workspace]
+"#;
