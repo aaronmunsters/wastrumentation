638bf68996def5f911079effd9613900d14b38d7
aaronmunsters <aaronmunstersbxl@gmail.com>
2024-11-15 13:38:53 +0100

    Update instruction-mix analysis

--- benchmarking-node/input-analyses/rust/instruction-mix/src/lib.rs
+++ benchmarking-node/input-analyses/rust/instruction-mix/src/lib.rs
@@ -1,7 +1,4 @@
 #![no_std]
-
-extern crate wastrumentation_rs_stdlib;
-
 use strum::EnumCount;
 use wastrumentation_rs_stdlib::*;
 
@@ -53,30 +50,32 @@ enum InstructionIndex {
     LoopPost,
 }
 
-advice! { if_                (c: PathContinuation, _ic: IfThenElseInputCount, _ia: IfThenElseArity   , _l: Location) { inc_instr(InstructionIndex::If);               c                         } }
-advice! { if_post            (                                                                         _l: Location) { inc_instr(InstructionIndex::IfPost);                                     } }
-advice! { if_then            (c: PathContinuation, _ic: IfThenInputCount, _ia: IfThenArity           , _l: Location) { inc_instr(InstructionIndex::IfThen);           c                         } }
-advice! { if_then_post       (                                                                         _l: Location) { inc_instr(InstructionIndex::IfThenPost);                                 } }
-advice! { br                 (_l: BranchTargetLabel                                                  , _l: Location) { inc_instr(InstructionIndex::Br);                                         } }
-advice! { br_if              (c : ParameterBrIfCondition, _l : ParameterBrIfLabel                    , _l: Location) { inc_instr(InstructionIndex::BrIf);             c                         } }
-advice! { br_table           (bt: BranchTableTarget, _e: BranchTableEffective, _d: BranchTableDefault, _l: Location) { inc_instr(InstructionIndex::BrIfTable);        bt                        } }
-advice! { select             (c: PathContinuation                                                    , _l: Location) { inc_instr(InstructionIndex::Select);           c                         } }
-advice! { call pre           (_t : FunctionIndex                                                     , _l: Location) { inc_instr(InstructionIndex::CallPre);                                    } }
-advice! { call post          (_t : FunctionIndex                                                     , _l: Location) { inc_instr(InstructionIndex::CallPost);                                   } }
-advice! { call_indirect pre  (t: FunctionTableIndex, _f: FunctionTable                               , _l: Location) { inc_instr(InstructionIndex::CallIndirectPre);  t                         } }
-advice! { call_indirect post (_t: FunctionTable                                                      , _l: Location) { inc_instr(InstructionIndex::CallIndirectPost);                           } }
-advice! { unary generic      (opt: UnaryOperator, opnd: WasmValue                                    , _l: Location) { inc_instr(InstructionIndex::Unary);            opt.apply(opnd)           } }
-advice! { binary generic     ( opt: BinaryOperator, l_opnd: WasmValue, r_opnd: WasmValue             , _l: Location) { inc_instr(InstructionIndex::Binary);           opt.apply(l_opnd, r_opnd) } }
-advice! { drop               (                                                                         _l: Location) { inc_instr(InstructionIndex::Drop);                                       } }
-advice! { return_            (                                                                         _l: Location) { inc_instr(InstructionIndex::Return);                                     } }
-advice! { const_ generic     (v: WasmValue                                                           , _l: Location) { inc_instr(InstructionIndex::Const);            v                         } }
-advice! { local generic      (v: WasmValue, _i: LocalIndex, _l: LocalOp                              , _l: Location) { inc_instr(InstructionIndex::Local);            v                         } }
-advice! { global generic     (v: WasmValue, _i: GlobalIndex, _g: GlobalOp                            , _l: Location) { inc_instr(InstructionIndex::Global);           v                         } }
-advice! { load generic       (i: LoadIndex, o: LoadOffset, op: LoadOperation                         , _l: Location) { inc_instr(InstructionIndex::Load);             op.perform(&i, &o)        } }
-advice! { store generic      (i: StoreIndex, v: WasmValue, o: StoreOffset, op: StoreOperation        , _l: Location) { inc_instr(InstructionIndex::Store);            op.perform(&i, &v, &o);   } }
-advice! { memory_size        (s: WasmValue, _i: MemoryIndex                                          , _l: Location) { inc_instr(InstructionIndex::MemorySize);       s                         } }
-advice! { memory_grow        (a: WasmValue, i: MemoryIndex                                           , _l: Location) { inc_instr(InstructionIndex::MemoryGrow);       i.grow(a)                 } }
-advice! { block pre          (_bi: BlockInputCount, _ba: BlockArity                                  , _l: Location) { inc_instr(InstructionIndex::BlockPre);                                   } }
-advice! { block post         (                                                                         _l: Location) { inc_instr(InstructionIndex::BlockPost);                                  } }
-advice! { loop_ pre          (_li: LoopInputCount, _la: LoopArity                                    , _l: Location) { inc_instr(InstructionIndex::LoopPre);                                    } }
-advice! { loop_ post         (                                                                         _l: Location) { inc_instr(InstructionIndex::LoopPost);                                   } }
+advice! {
+    if_then_else       (c: PathContinuation, _ic: IfThenElseInputCount, _ia: IfThenElseArity   , _l: Location) { inc_instr(InstructionIndex::If);               c                         }
+    if_then_else_post  (                                                                         _l: Location) { inc_instr(InstructionIndex::IfPost);                                     }
+    if_then            (c: PathContinuation, _ic: IfThenInputCount, _ia: IfThenArity           , _l: Location) { inc_instr(InstructionIndex::IfThen);           c                         }
+    if_then_post       (                                                                         _l: Location) { inc_instr(InstructionIndex::IfThenPost);                                 }
+    br                 (_l: BranchTargetLabel                                                  , _l: Location) { inc_instr(InstructionIndex::Br);                                         }
+    br_if              (c : ParameterBrIfCondition, _l : ParameterBrIfLabel                    , _l: Location) { inc_instr(InstructionIndex::BrIf);             c                         }
+    br_table           (bt: BranchTableTarget, _e: BranchTableEffective, _d: BranchTableDefault, _l: Location) { inc_instr(InstructionIndex::BrIfTable);        bt                        }
+    select             (c: PathContinuation                                                    , _l: Location) { inc_instr(InstructionIndex::Select);           c                         }
+    call pre           (_t : FunctionIndex                                                     , _l: Location) { inc_instr(InstructionIndex::CallPre);                                    }
+    call post          (_t : FunctionIndex                                                     , _l: Location) { inc_instr(InstructionIndex::CallPost);                                   }
+    call_indirect pre  (t: FunctionTableIndex, _f: FunctionTable                               , _l: Location) { inc_instr(InstructionIndex::CallIndirectPre);  t                         }
+    call_indirect post (_t: FunctionTable                                                      , _l: Location) { inc_instr(InstructionIndex::CallIndirectPost);                           }
+    unary              (opt: UnaryOperator, opnd: WasmValue                                    , _l: Location) { inc_instr(InstructionIndex::Unary);            opt.apply(opnd)           }
+    binary             ( opt: BinaryOperator, l_opnd: WasmValue, r_opnd: WasmValue             , _l: Location) { inc_instr(InstructionIndex::Binary);           opt.apply(l_opnd, r_opnd) }
+    drop               (                                                                         _l: Location) { inc_instr(InstructionIndex::Drop);                                       }
+    return_            (                                                                         _l: Location) { inc_instr(InstructionIndex::Return);                                     }
+    const_             (v: WasmValue                                                           , _l: Location) { inc_instr(InstructionIndex::Const);            v                         }
+    local              (v: WasmValue, _i: LocalIndex, _l: LocalOp                              , _l: Location) { inc_instr(InstructionIndex::Local);            v                         }
+    global             (v: WasmValue, _i: GlobalIndex, _g: GlobalOp                            , _l: Location) { inc_instr(InstructionIndex::Global);           v                         }
+    load               (i: LoadIndex, o: LoadOffset, op: LoadOperation                         , _l: Location) { inc_instr(InstructionIndex::Load);             op.perform(&i, &o)        }
+    store              (i: StoreIndex, v: WasmValue, o: StoreOffset, op: StoreOperation        , _l: Location) { inc_instr(InstructionIndex::Store);            op.perform(&i, &v, &o);   }
+    memory_size        (s: WasmValue, _i: MemoryIndex                                          , _l: Location) { inc_instr(InstructionIndex::MemorySize);       s                         }
+    memory_grow        (a: WasmValue, i: MemoryIndex                                           , _l: Location) { inc_instr(InstructionIndex::MemoryGrow);       i.grow(a)                 }
+    block pre          (_bi: BlockInputCount, _ba: BlockArity                                  , _l: Location) { inc_instr(InstructionIndex::BlockPre);                                   }
+    block post         (                                                                         _l: Location) { inc_instr(InstructionIndex::BlockPost);                                  }
+    loop_ pre          (_li: LoopInputCount, _la: LoopArity                                    , _l: Location) { inc_instr(InstructionIndex::LoopPre);                                    }
+    loop_ post         (                                                                         _l: Location) { inc_instr(InstructionIndex::LoopPost);                                   }
+}
