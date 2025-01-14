0ee552a3a2c2167d1b7c5f50fde6b955d5fd9181
aaronmunsters <aaronmunstersbxl@gmail.com>
2025-01-14 13:00:39 +0100

    Remove bash scripts for memo analysis

--- analysis-memoization-evaluation/3_Find_Pure_Functions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd working-directory
-cargo run --package wastrumentation-static-analysis -- \
-    --input-program-path *.wasm \
-    --output-path pure-function-indices.json \
-    --minimum-body 1
