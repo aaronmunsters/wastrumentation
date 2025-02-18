b9f1a35ecdcdd97c5fdbc7132486d044d31fc5b9
aaronmunsters <aaronmunstersbxl@gmail.com>
2025-02-18 17:16:49 +0100

    Update configuration

--- benchmarking-node/wasmtime-benchmarks/wrapper.py
+++ benchmarking-node/wasmtime-benchmarks/wrapper.py
@@ -27,36 +27,36 @@ result_file_field_names = [
 ]
 
 
-TIMEOUT = 1
+TIMEOUT_IN_SECONDS = 1 * 60 * 5
 TIMEOUT_THRESHOLD = 2
-RUNS = 5
+RUNS = 30
 INPUT_PROGRAMS = [
     # "commanderkeen",   # CRASH ON Apply Silicon M2
     # "sandspiel",       # CRASH ON Apply Silicon M2
     'jqkungfu',        # 12666 ns
     'game-of-life',    # 15417 ns
-    # "rtexpacker",      # 18166 ns
-    # "factorial",       # 23500 ns
-    # "rtexviewer",      # 20750 ns
-    # "ffmpeg",          # 31084 ns
-    # "figma-startpage", # 50334 ns
-    # "parquet",         # 90250 ns
-    # "hydro",           # 94083 ns
-    # "pacalc",          # 258750 ns
-    # "riconpacker",     # 434000 ns
-    # "sqlgui",          # 649125 ns
-    # "jsc",             # 2401333 ns
-    # "boa",             # 3646083 ns
-    # "guiicons",        # 17659125 ns
-    # "rguistyler",      # 16036084 ns
-    # "rguilayout",      # 18067291 ns
-    # "rfxgen",          # 18035166 ns
-    # "bullet",          # 26375500 ns
-    # "pathfinding",     # 386305041 ns
-    # "funky-kart",      # 39554500 ns
-    # "fib",             # 2386120708 ns
-    # "multiplyInt",     # 2683662583 ns
-    # "mandelbrot",      # 3032975916 ns
+    'rtexpacker',      # 18166 ns
+    'factorial',       # 23500 ns
+    'rtexviewer',      # 20750 ns
+    'ffmpeg',          # 31084 ns
+    'figma-startpage', # 50334 ns
+    'parquet',         # 90250 ns
+    'hydro',           # 94083 ns
+    'pacalc',          # 258750 ns
+    'riconpacker',     # 434000 ns
+    'sqlgui',          # 649125 ns
+    'jsc',             # 2401333 ns
+    'boa',             # 3646083 ns
+    'guiicons',        # 17659125 ns
+    'rguistyler',      # 16036084 ns
+    'rguilayout',      # 18067291 ns
+    'rfxgen',          # 18035166 ns
+    'bullet',          # 26375500 ns
+    'pathfinding',     # 386305041 ns
+    'funky-kart',      # 39554500 ns
+    'fib',             # 2386120708 ns
+    'multiplyInt',     # 2683662583 ns
+    'mandelbrot',      # 3032975916 ns
     'multiplyDouble',  # 7099443958 ns
 ]
 
@@ -86,11 +86,11 @@ for (platform, analysis, benchmarks_directory) in [
                         '-c',
                         f'cargo run --release -- --platform {platform} --input-program {input_program} --input-program-path {input_program_path} --analysis {analysis}',
                     ],
-                    timeout=TIMEOUT,
+                    timeout=TIMEOUT_IN_SECONDS,
                 )
 
             except subprocess.TimeoutExpired:
-                logging.warning(f'[platform:{platform},analysis:{analysis},benchmark:{input_program},runtime:{iteration}] timeout - {TIMEOUT}')
+                logging.warning(f'[platform:{platform},analysis:{analysis},benchmark:{input_program},runtime:{iteration}] timeout - {TIMEOUT_IN_SECONDS}')
 
                 with open('results.csv', 'a+') as results_file:
                     results_writer = csv.DictWriter(results_file, fieldnames=result_file_field_names)
@@ -103,7 +103,7 @@ for (platform, analysis, benchmarks_directory) in [
                     csv_values[KEY_completion_time] = 0
                     csv_values[KEY_time_unit] = 'ms'
                     csv_values[KEY_timeout] = True
-                    csv_values[KEY_timeout_amount] = TIMEOUT
+                    csv_values[KEY_timeout_amount] = TIMEOUT_IN_SECONDS
 
                     # Write row
                     results_writer.writerow(csv_values)
