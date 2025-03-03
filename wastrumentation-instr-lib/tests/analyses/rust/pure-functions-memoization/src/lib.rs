e5fdbabd1aaea66f7182c7f7a89a9c4bd8739ab9
aaronmunsters <aaronmunstersbxl@gmail.com>
2025-03-03 16:57:00 +0100

    Cleanup memoization to use heapless

--- wastrumentation-instr-lib/tests/analyses/rust/pure-functions-memoization/src/lib.rs
+++ wastrumentation-instr-lib/tests/analyses/rust/pure-functions-memoization/src/lib.rs
@@ -1,23 +1,24 @@
-extern crate wastrumentation_rs_stdlib;
+#![no_std]
 
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-use std::sync::atomic::AtomicUsize;
-use std::sync::Mutex;
+use heapless::{FnvIndexMap, Vec};
 use wastrumentation_rs_stdlib::{advice, MutDynArgs, MutDynResults, WasmFunction, WasmValue};
 
 // Global cache structure using a Mutex and AtomicUsize for thread safety
-lazy_static! {
-    static ref CACHE: Mutex<HashMap<CacheKey, Vec<WasmValue>>> = Mutex::new(HashMap::new());
-    static ref CACHE_SIZE: AtomicUsize = AtomicUsize::new(0);
-}
+const CACHE_CAPACITY: usize = 1024;
+const CACHED_VALUES_MAX_CAPACITY: usize = 10;
+const CACHED_RESULTS_MAX_CAPACITY: usize = 10;
+
+static mut CACHE: FnvIndexMap<
+    CacheKey,
+    Vec<WasmValue, CACHED_RESULTS_MAX_CAPACITY>,
+    CACHE_CAPACITY,
+> = FnvIndexMap::new();
+static mut CACHE_HITS: i32 = 0;
 
 #[no_mangle]
 pub extern "C" fn CACHE_SIZE_REPORT() -> i32 {
-    CACHE.lock().unwrap().len() as i32
+    unsafe { (*(&raw mut CACHE)).len() as i32 }
 }
-
-static mut CACHE_HITS: i32 = 0;
 
 #[no_mangle]
 pub extern "C" fn CACHE_HIT_REPORT() -> i32 {
@@ -40,31 +41,46 @@ fn to_bit_pattern(value: WasmValue) -> BitPattern {
 #[derive(Eq, PartialEq, Hash)]
 struct CacheKey {
     instr_f_idx: i32,
-    args: Vec<BitPattern>,
+    args: Vec<BitPattern, CACHED_VALUES_MAX_CAPACITY>,
 }
 
 fn cache_hit(key: &CacheKey) -> bool {
-    CACHE.lock().unwrap().contains_key(&key)
+    unsafe { (*(&raw mut CACHE)).contains_key(&key) }
 }
 
 fn cache_retrieve(key: &CacheKey, results: &mut MutDynResults) {
-    let cache = CACHE.lock().unwrap();
-    let cached_results = cache.get(&key).unwrap();
+    let cached_results = unsafe { (*(&raw mut CACHE)).get(&key).unwrap() };
     cached_results
         .into_iter()
         .enumerate()
         .for_each(|(index, wasm_value)| {
-            results.set_res(i32::try_from(index).unwrap(), wasm_value.to_owned())
+            results.set_res(i32::try_from(index).unwrap(), wasm_value.clone())
         });
 }
 
 fn cache_insert(key: CacheKey, results: &MutDynResults) {
-    let cached_results: Vec<WasmValue> = results.ress_iter().collect();
+    let cached_results: Vec<WasmValue, CACHED_RESULTS_MAX_CAPACITY> = results.ress_iter().collect();
     assert_eq!(cached_results.len(), usize::try_from(results.resc).unwrap());
-    CACHE.lock().unwrap().insert(key, cached_results);
+    unsafe {
+        (*(&raw mut CACHE))
+            .insert(key, cached_results)
+            .map_err(|_| ())
+            .unwrap();
+    }
 }
 
 advice! { apply (func: WasmFunction, args: MutDynArgs, results: MutDynResults) {
+        // If args / results would not fit in cache, continue as normal
+        let too_many_args = args.argc > CACHED_VALUES_MAX_CAPACITY as i32;
+        let too_many_ress = results.resc > CACHED_RESULTS_MAX_CAPACITY as i32;
+        let cache_full = unsafe { (*(&raw mut CACHE)).len() } == CACHE_CAPACITY;
+
+        if  too_many_args || too_many_ress || cache_full {
+            func.apply();
+            return;
+        };
+
+        // Construct cache key
         let key = CacheKey {
             instr_f_idx: func.instr_f_idx,
             args: args.args_iter().map(to_bit_pattern).collect(),
